04a860a6d02075c6509a5a788221012c1fe7ca88
Pesy Wu <admin@bili33.top>
2021-01-26 23:36:39 +0800

    Update GetNewCase.py

--- GetNewCase.py
+++ GetNewCase.py
@@ -12,6 +12,6 @@ def GetNew(csrf,sessdata):
     }
     data=r.post(url,headers=headers,params=params)
     dataloads=js.loads(data.text)
-    if(dataloads['code']==25014): return True
+    if(dataloads['code']==25014 or dataloads['code']=25008): return True
     result=dataloads['data']['id']
     return result
